190b9d91282a24f7cddecede20d8a5be08868ede
Muhammad Muhsin bin Mahdzir <74172640+Muhsin99@users.noreply.github.com>
2020-11-10 12:13:05 +0800

    Add files via upload

---
+++ SimpleQuiz(S52071).py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def class Square:
+    side = 1.0
+    def__init__(side)
+    self.side = side;
+    
+    def setSide(self):
+        if self.side < 0:
+            self.side = 0
+            else:
+                self.side=self
+
